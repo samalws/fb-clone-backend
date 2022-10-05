4a30df2b51fc9059490b375ac6dd7c5ea302437d
Sam Alws <sam@samalws.com>
2022-10-05 14:41:27 -0500

    flesh out a lot of backend stuff

--- index.js
+++ index.js
@@ -2,63 +2,172 @@ const fs = require("fs")
 const mongodb = require("mongodb")
 const graphql = require("graphql")
 
-class User {
-  constructor(id) {
-    this.id = id
-    this.username = "Username"
-    this.name = "Name"
-    this.pfpLink = "PfpLink"
-    this.isFriendReqIn = false
-    this.isFriendReqOut = false
+// TODO tok stuff, acct privacy stuff
+
+var db
+
+function user(tok, id) {
+  var vals
+  function getVals() {
+    vals = (vals !== undefined) ? vals : db.collection("users").findOne({ _id: id }, { projection: { _id: 0 } })
+    return vals
   }
-  friends() {
-    this.friends = [new User("friendIDA"), new User("friendIDB")]
-    return this.friends
-  }
-  posts() {
-    this.posts = [new Post("postIDA"), new Post("postIDB")]
-    return this.posts
-  }
-  replies() {
-    this.replies = [new Reply("replyIDA"), new Reply("replyIDB")]
-    return this.replies
+  const getField = (f) => getVals().then((v) => v[f])
+  return {
+    id,
+    username: () => getField("username"),
+    name: () => getField("name"),
+    pfpLink: () => getField("pfpLink"),
+    isFriendReqIn: false, // TODO
+    isFriendReqOut: false, // TODO
+    friends: () => userFriends(tok, id),
+    posts: () => userPosts(tok, id),
+    replies: () => userReplies(tok, id),
   }
 }
 
-class Post {
-  constructor(id) {
-    this.id = id
-    this.timestamp = "now lol"
-    this.poster = new User("posterID")
-    this.message = "i love sneed!"
-    this.likes = 1000
-    this.liked = true
+async function lookupUser(tok, query, projection) {
+  const lookuped = await db.collection("users").findOne(query, { projection })
+  if (lookuped === null) return undefined
+  const retVal = Object.assign({}, lookuped, query)
+  retVal.id = retVal._id
+  retVal.isFriendReqIn = false // TODO
+  retVal.isFriendReqOut = false // TODO
+  retVal.friends = () => userFriends(tok, retVal.id)
+  retVal.posts = () => userPosts(tok, retVal.id),
+  retVal.replies = () => userReplies(tok, retVal.id)
+  return retVal
+}
+
+function post(tok, id) {
+  var vals
+  function getVals() {
+    vals = (vals !== undefined) ? vals : db.collection("users").findOne({ _id: id }, { projection: { _id: 0 } })
+    return vals
   }
-  replies() {
-    this.replies = [new Reply("replyIDC"), new Reply("replyIDD")]
-    return this.replies
+  const getField = (f) => getVals().then((v) => v[f])
+  return {
+    id,
+    timestamp: () => getField("timestamp"),
+    poster: () => user(tok, getField("poster")),
+    message: () => getField("message"),
+    likes: () => getLikes(tok, id),
+    liked: () => getLiked(tok, id),
+    replies: () => postReplies(tok, id),
   }
 }
 
-class Reply {
-  constructor(id) {
-    this.id = id
-    this.timestamp = "now lol"
-    this.poster = new User("posterID")
-    this.message = "i love sneed!"
-    this.likes = 1000
-    this.liked = true
-    this.replyTo = new Post("replied to")
+async function lookupPost(tok, query, projection) {
+  const lookuped = await db.collection("posts").findOne(query, { projection })
+  if (lookuped === null) return undefined
+  const retVal = Object.assign({}, lookuped, query)
+  retVal.id = retVal._id
+  retVal.poster = () => user(tok, retVal.poster)
+  retVal.likes = () => getLikes(tok, retVal.id)
+  retVal.liked = () => getLiked(tok, retVal.id)
+  retVal.replies = () => postReplies(tok, retVal.id)
+  return retVal
+}
+
+function reply(tok, id) {
+  var vals
+  function getVals() {
+    vals = (vals !== undefined) ? vals : db.collection("users").findOne({ _id: id }, { projection: { _id: 0 } })
+    return vals
+  }
+  const getField = (f) => getVals().then((v) => v[f])
+  return {
+    id,
+    timestamp: () => getField("timestamp"),
+    poster: () => user(tok, getField("poster")),
+    message: () => getField("message"),
+    likes: () => getLikes(tok, id),
+    liked: () => getLiked(tok, id),
+    replyTo: () => post(tok, getField("replyTo")),
   }
 }
 
-const schemaContents = fs.readFileSync("backendSchema.gql", { encoding: "utf8" })
-const schema = graphql.buildSchema(schemaContents)
+async function lookupReply(tok, query, projection) {
+  const lookuped = await db.collection("replies").findOne(query, { projection })
+  if (lookuped === null) return undefined
+  const retVal = Object.assign({}, lookuped, query)
+  retVal.id = retVal._id
+  retVal.poster = () => user(tok, retVal.poster)
+  retVal.likes = () => getLikes(tok, retVal.id)
+  retVal.liked = () => getLiked(tok, retVal.id)
+  const oldReplyTo = retVal.replyTo
+  retVal.replyTo = () => post(tok, oldReplyTo)
+  return retVal
+}
 
-const source = '{ myUser(tok: "asdf") { id, username, name, pfpLink, isFriendReqIn, isFriendReqOut, friends { friends { id } }, posts { id, replies { id } } } }'
+async function userFriends(tok, id) {
+  const respA = await db.collection("friends").find({ personA: id }, { projection: { _id: 0, personB: 1 }})
+  const listA = await respA.toArray()
+  const respB = await db.collection("friends").find({ personB: id }, { projection: { _id: 0, personA: 1 }})
+  const listB = await respB.toArray()
+  const list = listA.map(({ personB }) => personB).concat(listB.map(({ personA }) => personA))
+  return list.map((uid) => user(tok, uid))
+}
+
+async function userPosts(tok, id) {
+  const resp = await db.collection("posts").find({ poster: id }, { projection: { _id: 1 }})
+  const list = await resp.toArray()
+  return list.map(({ _id }) => post(tok, _id))
+}
+
+async function userReplies(tok, id) {
+  const resp = await db.collection("replies").find({ poster: id }, { projection: { _id: 1 }})
+  const list = await resp.toArray()
+  return list.map(({ _id }) => reply(tok, _id))
+}
+
+async function postReplies(tok, id) {
+  const resp = await db.collection("replies").find({ replyTo: id }, { projection: { _id: 1 }})
+  const list = await resp.toArray()
+  return list.map(({ _id }) => reply(tok, _id))
+}
+
+async function getLikes(tok, id) {
+  const retVal = await db.collection("likes").count({ post: id })
+  return retVal
+}
+
+async function getLiked(tok, id) {
+  const retVal = await db.collection("likes").findOne({ liker: tok, post: id }, { projection: { _id: 0, liker: 0, post: 0 }})
+  return retVal !== null
+}
 
 const rootValue = {
-  myUser: ({ tok }) => new User("myID")
+  myUser: ({ tok }) => user(new mongodb.ObjectId(tok), new mongodb.ObjectId(tok)),
+  lookupUserId: ({ tok, id }) => lookupUser(new mongodb.ObjectId(tok), { _id: new mongodb.ObjectId(id) }, { _id: 0 }),
+  lookupUsername: ({ tok, username }) => lookupUser(new mongodb.ObjectId(tok), { username }, { username: 0 }),
+  lookupPostId: ({ tok, id }) => lookupPost(new mongodb.ObjectId(tok), { _id: new mongodb.ObjectId(id) }, { _id: 0 }),
+  lookupReplyId: ({ tok, id }) => lookupReply(new mongodb.ObjectId(tok), { _id: new mongodb.ObjectId(id) }, { _id: 0 }),
+  feed: ({ tok, pageNum }) => [], // TODO
+  login: ({ id, pwHashPreSalt }) => id, // TODO
+
+  setFriendStatus: ({ tok, id, val }) => false, // TODO
+  setLike: ({ tok, id, like }) => false, // TODO
+  makePost: ({ tok, message }) => null, // TODO
+  reply: ({ tok, id, message }) => null, // TODO
+  setAcctPrivacy: ({ tok, friendOnly }) => false, // TODO
+  setAcctPassword: ({ tok, pwHashPreSalt }) => false, // TODO
 }
 
-graphql.graphql({ schema, source, rootValue }).then((ret) => console.log(JSON.stringify(ret)))
+async function main() {
+  const uri = process.env.MONGO_URI
+  const mongoClient = new mongodb.MongoClient(uri)
+  await mongoClient.connect()
+  db = mongoClient.db("fb-clone")
+  console.log("connected!")
+
+  const schemaContents = fs.readFileSync("backendSchema.gql", { encoding: "utf8" })
+  const schema = graphql.buildSchema(schemaContents)
+
+  // example query
+  const source = '{ lookupUsername(tok: "633c86feaf773c87606e08fc", username: "Username0") { id, username, name, pfpLink, isFriendReqIn, isFriendReqOut, friends { id, friends { id } }, posts { id, likes, liked, replies { likes, liked, id } } } }'
+
+  graphql.graphql({ schema, source, rootValue }).then((ret) => console.log("FINAL VALUE", JSON.stringify(ret)))
+}
+
+main()
